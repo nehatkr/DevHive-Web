c6ea9f18945c213b40e0acc54d4016fd5ae51695
Nehatkr <nehathakur2308@gmail.com>
2025-10-27 15:09:16 +0530

    feture: created a page of connection request and connections

--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -1,11 +1,66 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { BASE_URL } from "../utils/constants";
+import { addRequests } from "../utils/requestSlice";
 
 const Requests = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const dispatch = useDispatch();
+  const requests = useSelector((store) => store.requests);
+  const fetchRequests = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/user/requests/received", {
+        withCredentials: true,
+      });
+      dispatch(addRequests(res.data.data));
+    } catch (err) {
+      // err
+    }
+  };
 
-export default Requests
+  useEffect(() => {
+    fetchRequests();
+  }, []);
+
+  if (!requests) return;
+  if (requests.length === 0) return <h1>No Requests found!</h1>;
+
+  return (
+    <div className="text-center my-10">
+      <h1 className="text-bold text-white font-bold text-3xl">Connection Requests</h1>
+      {requests.map((request) => {
+        //here we take one request and extract all the fields
+        const { _id, firstName, lastName, photoUrl, age, gender, about } =
+          request.fromUserId;
+
+        return (
+          <div
+            key={_id}
+            className="flex justify-between items-center m-4 p-4 rounded-lg bg-base-300 w-2/3 mx-auto"
+          >
+            <div>
+              <img
+                alt="photo"
+                className="w-20 h-20 rounded-full"
+                src={photoUrl}
+              />
+            </div>
+            <div className="text-left mx-4">
+              <h2 className="font-bold text-xl">
+                {firstName + " " + lastName}
+              </h2>
+              {age && gender && <p>{age + ", " + gender}</p>}
+              <p>{about}</p>
+            </div>
+            <div>
+              <button className="btn btn-primary mx-2">Reject</button>
+              <button className="btn btn-secondary mx-2">Accept</button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Requests;
